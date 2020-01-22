202bd9652a89f3acfbc854e3940b5c2d7411c5e7
vutDamon <41498233+Atsadavut1254@users.noreply.github.com>
2020-01-23 00:38:18 +0700

    edit app.js/app.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import fsci from './img/60year-fsci.png'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import fsci from "./img/60year-fsci.png";
 
 // import Route Tool
-import { Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
 
-import StudentForm from './Components/StudentForm'
+import Header from "./Components/header";
+import Footer from "./Components/footer";
+import Index from "./Components/Index";
+import Login from "./Components/Login";
+import StudentForm from "./Components/StudentForm";
 
-const Home = () => <h1>HOME</h1>
-const About = () => <h1>About</h1>
+
+const Home = () => <h1>HOME</h1>;
+const About = () => <h1>About</h1>;
 
 function App() {
   return (
-      <React.Fragment>
-          <div className="App">
-              <h1>NEW BUILD</h1>
-              <img src={fsci} width="100px"/>
-              <Route exact path="/" component={Home}/>
-              <Route exact path="/about" component={About}/>
-              <Route exact path="/std" component={StudentForm} />
-          </div>
-      </React.Fragment>
+    <React.Fragment>
+      <body>
+        <div className="App">
+          <Header />
+          <switch>
+            <Route exact path="/" component={Index} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/std" component={StudentForm} />
+          </switch>
+          <Footer />
+        </div>
+      </body>
+    </React.Fragment>
   );
 }
 
