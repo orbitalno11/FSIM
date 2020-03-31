eaf3e9e7c7da5ff2c46c1b5e843cd1eaaf402cd3
orbitalno11 <43870591+orbitalno11@users.noreply.github.com>
2020-03-31 10:45:27 +0700

    no

--- backend/api/admin_api_routes.py
+++ backend/api/admin_api_routes.py
@@ -125,7 +125,30 @@ def get_probation_student():
 @admin_bp.route('/alumni/readsheet/header', methods=['POST'])
 def read_alumni_survey():
     # this api require google sheet share url
+<<<<<<< HEAD
     sheet_url = request.form.get('sheet_url')
     header = read_sheet.read_table_header(sheet_url)
 
     return api_helper.return_response(header)
+=======
+    sheet_url = request.args.get('sheet_url')
+    header = request.args.get('header')
+    read_data = request.args.get('read_data')
+
+    if header:
+        print(header)
+        data = read_sheet.read_table_header(sheet_url)
+        return api_helper.return_response(data)
+
+    if read_data:
+        print(read_data)
+        data = read_sheet.read_sheet_data(sheet_url)
+        return api_helper.return_response(data)
+
+    value = {
+        'read_data': read_data,
+        'header': header
+    }
+
+    return api_helper.create_response(response_code=500, message="Read error, One of these Null", response=False, data=value)
+>>>>>>> parent of 012cc524... BE: add alumni read sheet survey analyze
