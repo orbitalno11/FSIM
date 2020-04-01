811387c53a4b1ff510be9eca7aeb1c60ff4d5ea4
orbitalno11 <43870591+orbitalno11@users.noreply.github.com>
2020-04-01 18:32:14 +0700

    add file

---
+++ frontend/src/redux/reducers/modalReducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+    modalOpen: null,
+    modalID: ""
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case 'OPEN_MODAL': {
+            return {
+                ...state,
+                modalOpen: action.modalOpen,
+                modalID: action.modalID
+            }
+        }
+
+        case "CLOSE_MODAL": {
+            return {
+                ...state,
+                modalOpen: action.modalOpen,
+                modalID: "None"
+            }
+        }
+
+        default:
+            return state
+    }
+}
