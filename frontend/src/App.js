4080ba4f1e9a8d5ceff3eff5e71abf919644fd10
orbitalno11 <43870591+orbitalno11@users.noreply.github.com>
2020-04-01 18:33:18 +0700

    add file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,19 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom'
 
 // general component
-// import Navbar from './components/general/Menu'
-// import Home from './components/layouts/Home'
+import Navbar from './components/general/Menu'
+import Home from './components/layouts/Home'
 
 // user component
-// import Admission from "./components/layouts/user/Admission";
-// import ActiveRecruitment from "./components/layouts/user/ActiveRecruitment";
-// import Alumni from "./components/layouts/user/Alumni";
-// import ActivityInformation from "./components/layouts/user/ActivityInformation";
+import Admission from "./components/layouts/user/Admission";
+import ActiveRecruitment from "./components/layouts/user/ActiveRecruitment";
+import Alumni from "./components/layouts/user/Alumni";
+import ActivityInformation from "./components/layouts/user/ActivityInformation";
 
 // admin component
-// import AdminHome from "./components/layouts/admin/Home";
-// import AdminActivity from "./components/layouts/admin/Activity";
-// import AdminNewStudent from "./components/layouts/admin/NewStudent"
+import AdminHome from "./components/layouts/admin/Home";
+import AdminActivity from "./components/layouts/admin/Activity";
+import AdminNewStudent from "./components/layouts/admin/NewStudent"
 
 class App extends Component {
   render() {
@@ -26,16 +26,16 @@ class App extends Component {
           {/* <Navbar/> */}
           <div className="App">
             <Switch>
-              {/* <Route exact path="/" component={Home}/>
+              <Route exact path="/" component={Home}/>
               <Route exact path="/admission" component={Admission}/>
               <Route exact path="/active" component={ActiveRecruitment}/>
               <Route exact path="/alumni" component={Alumni}/>
-              <Route exact path="/activity" component={ActivityInformation}/> */}
+              <Route exact path="/activity" component={ActivityInformation}/>
 
               {/*    admin    */}
-              {/* <Route exact path="/admin" component={AdminHome}/>
+              <Route exact path="/admin" component={AdminHome}/>
               <Route exact path="/admin/activity" component={AdminActivity} />
-              <Route exact path="/admin/newstudent" component={AdminNewStudent} /> */}
+              <Route exact path="/admin/newstudent" component={AdminNewStudent} />
             </Switch>
           </div>
         </Fragment>
