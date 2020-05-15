0269c05fd51fcb1748ab2a0a9fe2b1d99087cf85
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2020-05-15 22:56:29 +0700

    login

--- frontend/src/components/LoadingComponent.js
+++ frontend/src/components/LoadingComponent.js
@@ -3,14 +3,6 @@ import { Spinner } from 'react-bootstrap'
 import LoadingGif from '../assets/gif/Loading.gif'
 const LoadingComponent = () => (
     <div className="loading">
-        
-        {/* //     <div className="alertDialog">
-        //     <Spinner animation="border" role="status">
-        //         <span className="sr-only">Loading...</span>
-        //     </Spinner>
-
-        // </div>  */}
-    
          <img className="GifLoad" src={LoadingGif} alt="loading..." /> 
      </div>
 )
