25e9ece40c2999b43cbc0a2f0dcc1f22c4e77dfb
vutDamon <41498233+Atsadavut1254@users.noreply.github.com>
2020-05-20 19:35:34 +0700

    update redux student

---
+++ frontend/src/redux/action/adminStudentAction.js
@@ -0,0 +1,167 @@
+import * as types from '../types'
+
+import { startLoading, stopLoading } from './generalAction'
+import { openModal } from './modalAction'
+
+import axios from 'axios'
+
+// set select year
+const setSelectedYear = year => (
+    {
+        type: types.SET_STUDENT_SELECTED_YEAR,
+        year: year
+    }
+)
+
+// Load Student 
+const loadStudentstart = () => (
+    {
+        type: types.LOAD_STUDENT_DATA_START
+    }
+)
+
+const loadStudentSuccess = (studentData) => (
+    {
+        type: types.LOAD_STUDENT_DATA_SUCCESS,
+        studentData: activityData
+    }
+)
+
+const loadStudentFailed = (error) => (
+    {
+        type: types.LOAD_STUDENT_DATA_FAILED,
+        error: error
+    }
+)
+
+// student List 
+const loadStudentListStart = () => (
+    {
+        type: types.LOAD_STUDENT_LIST_START
+    }
+)
+
+const loadStudentListSuccess = (list) => (
+    {
+        type: types.LOAD_STUDENT_LIST_SUCCESS,
+        studentList: list
+    }
+)
+
+const loadStudentListFailed = (error) => (
+    {
+        type: types.LOAD_STUDENT_LIST_FAILED,
+        error: error
+    }
+)
+
+// add student
+const addStudentStart = () => (
+    {
+        type: types.ADD_STUDENT_START
+    }
+)
+
+const addStudentSuccess = (result) => (
+    {
+        type: types.ADD_STUDENT_SUCCESS,
+        actionResult: result
+    }
+)
+
+const addStudentFailed = (error, result) => (
+    {
+        type: types.ADD_STUDENT_FAILED,
+        actionResult: result,
+        error: error
+    }
+)
+
+//delete student 
+const deleteStudentStart = () => (
+    {
+        type: types.DELETE_STUDENT_START
+    }
+)
+
+const deleteStudentSuccess = (result) => (
+    {
+        type: types.DELETE_STUDENT_SUCCESS,
+        actionResult: result
+    }
+)
+
+const deleteStudentFailed = (error, result) => (
+    {
+        type: types.DELETE_STUDENT_FAILED,
+        actionResult: result,
+        error: error
+    }
+)
+
+export const selectYear = year => dispatch => {
+    dispatch(setSelectedYear(year))
+}
+
+export const getStudentData = dept_id => dispatch => {
+    dispatch(startLoading())
+    dispatch(loadStudentstart())
+
+    let studentData = {}
+
+    axios.get(`/student/department?dept_id=${dept_id}`)
+        .then(res => {
+            let data = res.data.data
+
+            if (Object.keys(data) < 1) {
+                dispatch(loadActivityFalied("Can not find data"))
+                dispatch(stopLoading())
+                return
+            }
+
+            let department = data['dept_name']
+            let branch = data['branch'][0]
+            let byYear = data['status_by_year'][0]
+            let byBranch = data['df_status_by_branch'][0]
+            let dept_id = dept_id
+
+            studentData = {
+                department: department,
+                branch: branch,
+                byYear: byYear,
+                byBranch: byBranch,
+                dept_id: dept_id
+            }
+
+            dispatch(loadStudentSuccess(studentData))
+            dispatch(stopLoading())
+        })
+        .catch(err => {
+            dispatch(loadStudentFailed(err))
+            dispatch(stopLoading())
+        })
+
+    export const getStudentList = () => dispatch => {
+        dispatch(startLoading())
+        dispatch(loadStudentListStart())
+
+        axios.get(`/admin/student/list`)
+            .then(res => {
+                let data = res.data.data
+
+                if (Object.keys(data) < 1) {
+                    dispatch(loadActivityListFalied("Can not find data"))
+                    dispatch(stopLoading())
+                    return
+                }
+
+                dispatch(loadStudentListSuccess(data))
+                dispatch(stopLoading())
+            })
+            .catch(err => {
+                dispatch(loadStudentListFailed(err))
+                dispatch(stopLoading())
+            })
+    }
+    
+}
---
+++ frontend/src/redux/reducers/adminStudentReducer.js
@@ -0,0 +1,84 @@
+import * as types from '../types'
+
+let year = new Date()
+year = year.getFullYear() + 543
+
+const initialState = {
+    studentData: null,
+    studentList: null,
+    yearList: null,
+    selectedYear: year,
+    actionResult: null,
+    error: null
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case types.SET_STUDENT_SELECTED_YEAR:
+            return {
+                ...state,
+                selectedYear: action.year
+            }
+        case types.LOAD_STUDENT_DATA_START:
+            return {
+                ...state
+            }
+        case types.LOAD_STUDENT_DATA_SUCCESS:
+            return {
+                ...state,
+                studentData: action.studentData
+            }
+        case types.LOAD_STUDENT_DATA_FAILED:
+            return {
+                ...state,
+                studentData: null,
+                error: action.error
+            }    
+        case types.LOAD_STUDENT_LIST_START:
+            return {
+                ...state
+            }
+        case types.LOAD_STUDENT_LIST_SUCCESS:
+            return {
+                ...state,
+                studentList: action.studentList
+            }
+        case types.LOAD_STUDENT_LIST_FAILED:
+            return {
+                ...state,
+                studentList: null,
+                error: action.error
+            }
+        case types.ADD_STUDENT_START:
+            return {
+                ...state
+            }
+        case types.ADD_STUDENT_SUCCESS:
+            return {
+                ...state,
+                actionResult: action.actionResult
+            }
+        case types.ADD_STUDENT_FAILED:
+            return {
+                ...state,
+                actionResult: action.actionResult,
+                error: action.error
+            }
+        case types.GET_STUDENT_YEAR_LIST_START:
+            return {
+                ...state
+            }
+        case types.GET_STUDENT_YEAR_LIST_SUCCESS:
+            return {
+                ...state,
+                yearList: action.yearList
+            }
+        case types.GET_STUDENT_YEAR_LIST_FAILED:
+            return {
+                ...state,
+                yearList: null,
+                error: action.error
+            }
+        default: return state
+    }
+}
